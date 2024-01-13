3101806f11b5d993a937a564639b6ba764e1f0c0
Abhijxxt14 <154517350+Abhijxxt14@users.noreply.github.com>
2024-01-13 11:14:21 +0530

    Update CMD1.java

--- CMD1.java
+++ CMD1.java
@@ -4,6 +4,11 @@ public class CMD1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int dividend = Integer.parseInt(args[0]);
+		int divisor = Integer.parseInt(args[1]);
+		int quotient = dividend/divisor;
+		int remainder = dividend%divisor;
+		System.out.println("Quotient = "+quotient+" and Remainder = "+remainder);
 
 	}
 
