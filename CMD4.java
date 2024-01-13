929f65fcb7a60ae2e882af809558cf8e4655a719
Abhijxxt14 <154517350+Abhijxxt14@users.noreply.github.com>
2024-01-13 11:17:19 +0530

    Update CMD4.java

--- CMD4.java
+++ CMD4.java
@@ -4,7 +4,9 @@ public class CMD4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+double t = Double.parseDouble(args[0]);
+		double value = Math.cos(5*t) + Math.sin(7*t);
+		System.out.println("Sum value is " + value);
 	}
 
 }
