24e234340a6ad34a3325b89c65bcba3908d3be40
Abhijxxt14 <154517350+Abhijxxt14@users.noreply.github.com>
2024-01-13 11:18:41 +0530

    Update CMD7.java

--- CMD7.java
+++ CMD7.java
@@ -4,7 +4,11 @@ public class CMD7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		boolean val = (a<=b*c?true:(b<=a*c?true:(c<=a*b?true:false)));
+		System.out.println(val);
 	}
 
 }
