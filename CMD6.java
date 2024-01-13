15e6d2c819a7666dfa2c2273137eee82eb3ee38d
Abhijxxt14 <154517350+Abhijxxt14@users.noreply.github.com>
2024-01-13 11:18:18 +0530

    Update CMD6.java

--- CMD6.java
+++ CMD6.java
@@ -4,7 +4,8 @@ public class CMD6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+char ch = args[0].charAt(0);
+		System.out.println("ASCII value of " + ch+" is "+(int)ch);
 	}
 
 }
