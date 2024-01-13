10d84405065e97add09f88eb4d6c4ad2437bff3a
Abhijxxt14 <154517350+Abhijxxt14@users.noreply.github.com>
2024-01-13 11:14:58 +0530

    Update CMD2.java

--- CMD2.java
+++ CMD2.java
@@ -4,7 +4,10 @@ public class CMD2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int n = Integer.parseInt(args[0]);
+		int p = Integer.parseInt(args[1]);
+		double power = Math.pow(n,p);
+		System.out.println(n+" power "+p+" = "+power);
 	}
 
 }
