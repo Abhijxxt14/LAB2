096ffeb2a62f7245b30143e88dc450e5dfb73d33
Abhijxxt14 <154517350+Abhijxxt14@users.noreply.github.com>
2024-01-13 11:16:25 +0530

    Update CMD3.java

--- CMD3.java
+++ CMD3.java
@@ -4,7 +4,12 @@ public class CMD3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int minR = Integer.parseInt(args[0]);
+		int maxR = Integer.parseInt(args[1]);
+		int fNumber = (int)(Math.random()*(maxR-minR+1)) + minR;
+		int sNumber = (int)(Math.random()*(maxR-minR+1)) + minR;
+		int sum = sNumber + fNumber;
+		System.out.println("Sum of two random integers "+fNumber+" and "+sNumber + "is"+ sum);
 	}
 
 }
